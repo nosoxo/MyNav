f055958a74b6015355bc23bca5e9370c9b54f555
nosoxo <loyo0801@gmail.com>
2022-03-02 21:00:36 +0800

    更新链接排序

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -34,39 +34,73 @@ class LinkController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function index (Request $request)
+    public function index(Request $request)
     {
-        if (!check_admin_auth ($this->module_name . '_' . __FUNCTION__)) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . '_' . __FUNCTION__)) {
+            return auth_error_return();
         }
-        if (request ()->wantsJson ()) {
-            $limit = $request->input ('limit', 15);
-            QueryWhere::defaultOrderBy ('links.sort', 'ASC')->setRequest ($request->all ());
-            $M = $this->repository->makeModel ()->select ('links.*');
-            QueryWhere::eq ($M, 'links.category_id');
-            QueryWhere::like ($M, 'links.title');
-            QueryWhere::like ($M, 'links.url');
-            QueryWhere::like ($M, 'links.description');
-            QueryWhere::orderBy ($M);
-            $M     = $M->paginate ($limit);
-            $count = $M->total ();
-            $data  = $M->items ();
-            foreach ($data as $key => $item) {
-                $data[ $key ]['category_name'] = Category::where ('id', $item->category_id)->value ('name');
+        if (request()->wantsJson()) {
+//            $limit = $request->input ('limit', 15);
+//            QueryWhere::defaultOrderBy ('links.sort', 'ASC')->setRequest ($request->all ());
+//            $M = $this->repository->makeModel ()->select ('links.*');
+//            QueryWhere::eq ($M, 'links.category_id');
+//            QueryWhere::like ($M, 'links.title');
+//            QueryWhere::like ($M, 'links.url');
+//            QueryWhere::like ($M, 'links.description');
+//            QueryWhere::orderBy ($M);
+//            $M     = $M->paginate ($limit);
+//            $count = $M->total ();
+//            $data  = $M->items ();
+//            foreach ($data as $key => $item) {
+//                $data[ $key ]['category_name'] = Category::where ('id', $item->category_id)->value ('name');
+//            }
+//            $result = [
+//                'count' => $count,
+//                'data'  => $data
+//            ];
+//
+//            return ajax_success_result ('成功', $result);
+            $limit = $request->input('limit', 15);
+            $links = Link::leftJoin('categories', 'categories.id', '=', 'category_id')->select('links.*', 'categories.name AS category_name', 'categories.sort AS category_sort')->orderBy('category_sort')->orderBy('sort');
+            if (array_get($request, 'searchParams')) {
+                $arr = json_decode(array_get($request, 'searchParams', []), true);
+                if (is_array($arr)) {
+                    foreach ($arr as $key => $val) {
+                        $searchParams[$key] = $val;
+                    }
+                }
+                $category_id = $searchParams['category_id'];
+                $title = $searchParams['title'];
+                $url = $searchParams['url'];
+                $description = $searchParams['description'];
+                if ($category_id) {
+                    $links->where('category_id', $category_id);
+                }
+                if ($title) {
+                    $links->where('title', 'like', "%$title%");
+                }
+                if ($url) {
+                    $links->where('url', 'like', "%$url%");
+                }
+                if ($description) {
+                    $links->where('description', 'like', "%$description%");
+                }
             }
-            $result = [
+            $links = $links->paginate($limit);
+            $count = $links->total();
+            $data = $links->items();
+            $links = [
                 'count' => $count,
-                'data'  => $data
+                'data' => $data
             ];
-
-            return ajax_success_result ('成功', $result);
+            return ajax_success_result('成功', $links);
 
         } else {
             $links = $this->repository->makeModel();
 
-            $categories = Category::orderBy('sort','asc')->get();
+            $categories = Category::orderBy('sort', 'asc')->get();
 
-            return view ('admin.' . $this->module_name . '.index', compact ('links','categories'));
+            return view('admin.' . $this->module_name . '.index', compact('links', 'categories'));
         }
     }
 
@@ -75,117 +109,120 @@ class LinkController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create (Request $request)
+    public function create(Request $request)
     {
-        if (!check_admin_auth ($this->module_name . '_create')) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . '_create')) {
+            return auth_error_return();
         }
         $_method = 'POST';
-        $categories   = Category::orderBy ('sort', 'asc')->get();
-        $link    = new Link();
-        return view ('admin.' . $this->module_name . '.add', compact ('link', 'categories', '_method'));
+        $categories = Category::orderBy('sort', 'asc')->get();
+        $link = new Link();
+        return view('admin.' . $this->module_name . '.add', compact('link', 'categories', '_method'));
     }
+
     /**
      * 提交添加
      *
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store (Request $request)
+    public function store(Request $request)
     {
-        $request->validate ([
+        $request->validate([
             'Link.category_id' => 'required',
-            'Link.title'  => 'required',
-            'Link.url'  => 'required',
-            'Link.sort'  => 'required',
-            'Link.flag'  => 'required',
+            'Link.title' => 'required',
+            'Link.url' => 'required',
+            'Link.sort' => 'required',
+            'Link.flag' => 'required',
         ], [], [
             'Link.category_id' => '所属分类',
-            'Link.title'  => '链接名称',
-            'Link.url'  => 'url',
-            'Link.sort'  => '排序',
-            'Link.flag'  => '私有',
+            'Link.title' => '链接名称',
+            'Link.url' => 'url',
+            'Link.sort' => '排序',
+            'Link.flag' => '私有',
         ]);
 
-        if (!check_admin_auth ($this->module_name . '_create')) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . '_create')) {
+            return auth_error_return();
         }
-        $input = $request->input ('Link');
+        $input = $request->input('Link');
         try {
-            $title       = trim ($input['title']);
-            $link = Link::where ('title', $title)->first ();
+            $title = trim($input['title']);
+            $link = Link::where('title', $title)->first();
             if ($link) {
-                return ajax_error_result ('链接名称[' . $title . ']已经存在');
+                return ajax_error_result('链接名称[' . $title . ']已经存在');
             }
-            $link = Link::create ([
+            $link = Link::create([
                 'category_id' => (int)$input['category_id'],
                 'title' => $input['title'],
                 'url' => $input['url'],
                 'description' => $input['description'],
                 'sort' => $input['sort'],
                 'flag' => $input['flag'],
-                ]);
+            ]);
 
             if ($link) {
-                Log::createLog (Log::EDIT_TYPE, '添加链接', $link->toArray (), $link->id, Link::class);
-                return ajax_success_result ('添加成功');
+                Log::createLog(Log::EDIT_TYPE, '添加链接', $link->toArray(), $link->id, Link::class);
+                return ajax_success_result('添加成功');
             } else {
-                return ajax_error_result ('添加失败');
+                return ajax_error_result('添加失败');
             }
         } catch (BusinessException $e) {
-            return ajax_error_result ($e->getMessage ());
+            return ajax_error_result($e->getMessage());
         }
     }
+
     /**
      * 修改链接
      *
      * @param \App\Models\Link $link
      * @return \Illuminate\Http\Response
      */
-    public function edit (Link $link)
+    public function edit(Link $link)
     {
-        if (!check_admin_auth ($this->module_name . ' edit')) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . ' edit')) {
+            return auth_error_return();
         }
         $_method = 'PUT';
 //        $categories = Category::all();
-        $categories   = Category::orderBy ('sort', 'asc')->get ();
+        $categories = Category::orderBy('sort', 'asc')->get();
 
 
-        return view ('admin.' . $this->module_name . '.add', compact ('link', '_method', 'categories'));
+        return view('admin.' . $this->module_name . '.add', compact('link', '_method', 'categories'));
     }
+
     /**
      * 提交修改
      *
      * @param \Illuminate\Http\Request $request
-     * @param \App\Models\link   $link
+     * @param \App\Models\link $link
      * @return \Illuminate\Http\Response
      */
-    public function update (Request $request, Link $link)
+    public function update(Request $request, Link $link)
     {
-        $request->validate ([
+        $request->validate([
             'Link.category_id' => 'required',
-            'Link.title'  => 'required',
-            'Link.url'  => 'required',
-            'Link.sort'  => 'required',
-            'Link.flag'  => 'required',
+            'Link.title' => 'required',
+            'Link.url' => 'required',
+            'Link.sort' => 'required',
+            'Link.flag' => 'required',
         ], [], [
             'Link.category_id' => '所属分类',
-            'Link.title'  => '链接名称',
-            'Link.url'  => 'url',
-            'Link.sort'  => '排序',
-            'Link.flag'  => '私有',
+            'Link.title' => '链接名称',
+            'Link.url' => 'url',
+            'Link.sort' => '排序',
+            'Link.flag' => '私有',
         ]);
 
-        if (!check_admin_auth ($this->module_name . '_create')) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . '_create')) {
+            return auth_error_return();
         }
-        $input = $request->input ('Link');
+        $input = $request->input('Link');
         try {
-            $title       = trim ($input['title']);
-            $check = Link::where ('title', $title)->where ('id', '<>', $link->id)->first ();
+            $title = trim($input['title']);
+            $check = Link::where('title', $title)->where('id', '<>', $link->id)->first();
             if ($check) {
-                return ajax_error_result ('链接名称[' . $title . ']已经存在，无需重复添加');
+                return ajax_error_result('链接名称[' . $title . ']已经存在，无需重复添加');
             }
             $link->fill([
                 'category_id' => (int)$input['category_id'],
@@ -197,13 +234,13 @@ class LinkController extends Controller
             ]);
             $link->save();
             if ($link) {
-                Log::createLog (Log::EDIT_TYPE, '修改链接', $link->toArray (), $link->id, Link::class);
-                return ajax_success_result ('更新成功');
+                Log::createLog(Log::EDIT_TYPE, '修改链接', $link->toArray(), $link->id, Link::class);
+                return ajax_success_result('更新成功');
             } else {
-                return ajax_error_result ('更新失败');
+                return ajax_error_result('更新失败');
             }
         } catch (BusinessException $e) {
-            return ajax_error_result ($e->getMessage ());
+            return ajax_error_result($e->getMessage());
         }
     }
 
@@ -213,45 +250,46 @@ class LinkController extends Controller
      * @param \App\Models\Link $link
      * @return \Illuminate\Http\Response
      */
-    public function show (Link $link)
+    public function show(Link $link)
     {
-        if (!check_admin_auth ($this->module_name.' show')) {
+        if (!check_admin_auth($this->module_name . ' show')) {
             return auth_error_return();
         }
-        $link->category=Category::where('id',$link->category_id)->value('name');
-        return view ('admin.' . $this->module_name . '.show', compact ('link'));
+        $link->category = Category::where('id', $link->category_id)->value('name');
+        return view('admin.' . $this->module_name . '.show', compact('link'));
     }
+
     /**
      * 删除链接
      *
      * @return \Illuminate\Http\Response
      * @throws BusinessException
      */
-    public function destroy ($id, Request $request)
+    public function destroy($id, Request $request)
     {
-        if (!check_admin_auth ($this->module_name . '_delete')) {
-            return auth_error_return ();
+        if (!check_admin_auth($this->module_name . '_delete')) {
+            return auth_error_return();
         }
-        $ids = $request->input ('ids', []);
+        $ids = $request->input('ids', []);
         if (empty($ids)) {
             $ids[] = $id;
         }
-        DB::beginTransaction ();
-        $ids   = (array)$ids;
-        $M     = $this->repository->makeModel ();
-        $lists = $M->whereIn ('id', $ids)->get ();
-        $num   = 0;
+        DB::beginTransaction();
+        $ids = (array)$ids;
+        $M = $this->repository->makeModel();
+        $lists = $M->whereIn('id', $ids)->get();
+        $num = 0;
         foreach ($lists as $item) {
             $log_title = '删除链接[' . ($item->category->title ?? '') . '->' . $item->title . ']记录';
 //            $check     = $this->repository->allowDelete ($item->id);
-            $ret = Link::destroy ($item->id);
-            if ($ret){
-                Log::createLog (Log::DELETE_TYPE, $log_title, $item, $item->id, Link::class);
+            $ret = Link::destroy($item->id);
+            if ($ret) {
+                Log::createLog(Log::DELETE_TYPE, $log_title, $item, $item->id, Link::class);
                 $num++;
             }
         }
-        DB::commit ();
-        return ajax_success_result ('成功删除' . $num . '条记录');
+        DB::commit();
+        return ajax_success_result('成功删除' . $num . '条记录');
     }
 
 
